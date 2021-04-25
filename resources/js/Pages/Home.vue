bf0923bd5508b03351f75ecebbab796b1dde41d9
net@Techino <nati2mulugeta@gmail.com>
2021-04-25 11:55:52 -0700

    initial breeze with voyager

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <Layout title="Home">
+        <div>
+            <h1>This is the landing page! {{ foo }}</h1>    
+        </div>   
+    </Layout>  
+</template>
+
+<script>
+import Layout from './Shared/Layout'
+
+export default {
+    props: ['foo'],
+    components: {
+        Layout,
+    }
+}
+</script>
